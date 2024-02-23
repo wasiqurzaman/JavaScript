68124028900b961742126a9eb8e79c526dd84c94
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-02-23 19:34:50 +0530

    added the data-type.js file

---
+++ src/data-types.js
@@ -0,0 +1,19 @@
+// typeof operator is used to know the type of data a variable is holding
+
+let hello;
+
+console.log(typeof hello);                     // undefined
+
+let jsIsFun = true;
+
+console.log(typeof jsIsFun);                   // boolean
+console.log(typeof false);                     // boolean
+
+console.log(typeof 34);                        // number
+console.log(typeof "Hello World");             // string
+
+jsIsFun = "yes";
+console.log(typeof jsIsFun);                   // string
+
+console.log(hello)
+console.log(typeof undefined);                 // undefined =>> undefined is both a type and a value
